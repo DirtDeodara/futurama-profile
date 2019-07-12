944faf5c46c7e1d92b09865a7ba894d5ac5ba285
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-12 16:14:49 -0700

    managed to create a profile and tested

--- public/index.js
+++ public/index.js
@@ -1,7 +1,1 @@
-const app = require('./lib/app');
 
-const PORT = process.env.PORT || 9999;
-
-app.listen(PORT, () => {
-  console.log(`Vigilantly listening on port ${PORT}`);
-});
---
+++ tests/futurama.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+
+const app = require('../lib/app');
+
+describe('futurama tests', () => {
+  it('can create a profile with POST', () => {
+    return request(app)
+      .post('/api/v1/profiles')
+      .send({ name: 'Dirt', favoriteCharacter: 'Bender' })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Dirt',
+          favoriteCharacter: 'Bender'
+        });
+      });
+  });
+});
